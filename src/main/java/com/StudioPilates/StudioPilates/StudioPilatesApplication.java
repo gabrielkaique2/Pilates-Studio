413e0f8cf6234208ab01e2e1f3af5d93697e6c94
Gabriel Kaique <gabrielkaique_contato@hotmail.com>
2025-06-24 22:49:18 -0300

    Starting Project

---
+++ src/main/java/com/StudioPilates/StudioPilates/StudioPilatesApplication.java
@@ -0,0 +1,13 @@
+package com.StudioPilates.StudioPilates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioPilatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioPilatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StudioPilates/StudioPilates/StudioPilatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudioPilates.StudioPilates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioPilatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2762369f12802b1f27fc1cb4e55c3034e2eb94f3
Gabriel Kaique <gabrielkaique_contato@hotmail.com>
2025-06-26 22:06:07 -0300

    Repository finished

---
+++ src/main/java/com/StudioPilates/StudioPilates/repository/Patient.java
@@ -0,0 +1,6 @@
+package com.StudioPilates.StudioPilates.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Patient extends JpaRepository<Patient, Long> {
+}
